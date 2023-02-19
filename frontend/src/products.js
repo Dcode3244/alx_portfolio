d93aeda2599a9b80e607a999ea4279d036384260
dcode3244 <dagimourgie111@gmail.com>
2023-02-19 15:52:08 +0300

    added homescreen and products component for displying products

---
+++ frontend/src/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Full Package',
+    image: '/images/cloth1.jpg',
+    description:
+      'Classy full package cloth, jacket, pants, shirts shoe and hat',
+    brand: 'Abyssinia',
+    category: 'Full',
+    price: 29.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Tshirt',
+    image: '/images/cloth2.jpg',
+    description:
+      'high quality black Tshirt',
+    brand: 'Abyssinia',
+    category: 'shirts',
+    price: 519.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Jacket',
+    image: '/images/cloth3.jpg',
+    description:
+      'High quality cozy and comfortable Jacket',
+    brand: 'Abyssinia',
+    category: 'jackets',
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Pant',
+    image: '/images/cloth4.jpg',
+    description:
+      'High quality pants, different color alternatives, very comfortable',
+    brand: 'Abyssinia',
+    category: 'pants',
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Tshirt',
+    image: '/images/cloth5.jpg',
+    description:
+      'Custom design, high quality shirt',
+    brand: 'Abyssinia',
+    category: 'shirts',
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Suit',
+    image: '/images/cloth6.jpg',
+    description:
+      'Classy suits',
+    brand: 'Abyssinia',
+    category: 'suits',
+    price: 29.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+]
+
+export default products
