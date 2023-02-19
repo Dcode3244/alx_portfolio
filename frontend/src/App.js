236d8556f612b11b3d6e54b4d7beffe2854b054a
dcode3244 <dagimourgie111@gmail.com>
2023-02-19 14:00:25 +0300

    added a navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <h1>Welcome to abyssinia</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to abyssinia</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
