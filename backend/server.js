9837fde9943bd10585f43ed790de81b430699111
dcode3244 <dagimourgie111@gmail.com>
2023-02-26 18:56:26 +0300

    configured server.js and routes/productRoutes.js to fetch data from database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
-
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -19,15 +18,7 @@ app.get('/status', (req, res) => {
     res.json({ 'status': 'OK' })
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id == req.params.id)
-    res.json(product)
-})
-
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000
 
