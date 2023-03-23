6eeb7a8e930cce314263aad86d21fc671e9653fa
dcode3244 <dagimourgie111@gmail.com>
2023-03-23 11:05:12 +0300

    added authorization middleware and login and profile endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
+import protect from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 import {
-    authUser
+    authUser, getUserProfile
 } from '../controllers/userControllers.js'
 
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
